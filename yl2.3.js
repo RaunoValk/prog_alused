d9978d5c09f2ddc7fd1e8e635bdf377c1a38802a
RV <rauno.valk@voco.ee>
2024-09-27 11:06:14 +0300

    Ylesanne 2.3

--- yl2.3.js
+++ yl2.3.js
@@ -4,3 +4,31 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+rl.question("Sisestage enda vanus: ", vanus=>{
+  rl.question("Sisestage enda sugu: ", sugu=>{
+    rl.question("Sisestage treeningu tüüp: ", tüüp=>{
+      let pulss
+      let minvahemik
+      let maxvahemik
+      if(sugu== "M" || sugu== "m"){
+        pulss= 220 - vanus} 
+        else if(sugu== "N" || sugu== "n"){
+        pulss= 206 - vanus * 0.88}
+        
+        if(tüüp == 1){
+          minvahemik=pulss * 0.5
+          maxvahemik=pulss * 0.7
+        }
+        else if(tüüp == 2){
+          minvahemik=pulss * 0.7
+          maxvahemik=pulss *0.8
+        } 
+        else if(tüüp == 3){
+          minvahemik=pulss * 0.8
+          maxvahemik=pulss *0.87
+       }
+       console.log(`Pulsisagedus peaks olema vahemikus ${Math.round(minvahemik)} ja ${Math.round(maxvahemik)}.`)
+          rl.close()
+        }) 
+        }) 
+})
