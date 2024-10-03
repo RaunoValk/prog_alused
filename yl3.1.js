10a97ba60a186b5abbd59bdb18f79f6f9718da6b
RV <rauno.valk@voco.ee>
2024-10-03 09:31:36 +0300

    Ylesanne3.1

---
+++ yl3.1.js
@@ -0,0 +1,19 @@
+const readline = require('node:readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Äratuste arv: " , äratus =>  {
+   const numäratus = parseInt(äratus, 10)
+   if(isNaN(numäratus)|| numäratus < 0 ){
+    rl.close()
+   } else{
+
+    
+    for (let i = 0; i < äratus; i++) {
+
+       console.log("Tõuse ja sära!")
+rl.close()
+}}})
